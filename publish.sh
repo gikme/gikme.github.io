fcb16398ddff5e6f55c7921d2204f7b011253d13
Vadim Lopatyuk <qnub.ru@gmail.com>
2015-02-01 00:30:38 +0600

    Again

--- publish.sh
+++ publish.sh
@@ -2,12 +2,12 @@
 
 GIT_ROOT="/var/www/gik.me/pelican_site"
 VIRTUALENV="/var/www/gik.me/.env/bin"
-PELICAN="${VIRTUAENV}/pelican"
-PY="${VIRTUAENV}/python"
-PIP="${VIRTUAENV}/pip"
-INPUTDIR="${GIT_ROOT}/site/content"
-OUTPUTDIR="${GIT_ROOT}/site/output"
-CONFFILE="${GIT_ROOT}/site/publishconf.py"
+PELICAN="$VIRTUAENV/pelican"
+PY="$VIRTUAENV/python"
+PIP="$VIRTUAENV/pip"
+INPUTDIR="$GIT_ROOT/site/content"
+OUTPUTDIR="$GIT_ROOT/site/output"
+CONFFILE="$GIT_ROOT/site/publishconf.py"
 
 
 cd $GIT_ROOT
