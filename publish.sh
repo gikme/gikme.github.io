e50dbe5546578523ee6db3baebb6b819459d60dd
Vadim Lopatyuk <qnub.ru@gmail.com>
2015-02-01 00:35:34 +0600

    copypaster - rules

--- publish.sh
+++ publish.sh
@@ -2,14 +2,14 @@
 
 GIT_ROOT="/var/www/gik.me/pelican_site"
 VIRTUALENV="/var/www/gik.me/.env/bin"
-PELICAN="$VIRTULAENV/pelican"
-PY="$VIRTULAENV/python"
-PIP="$VIRTULAENV/pip"
+PELICAN="$VIRTUALENV/pelican"
+PY="$VIRTUALENV/python"
+PIP="$VIRTUALENV/pip"
 INPUTDIR="$GIT_ROOT/site/content"
 OUTPUTDIR="$GIT_ROOT/site/output"
 CONFFILE="$GIT_ROOT/site/publishconf.py"
 
 
 cd $GIT_ROOT
-exec $PIP install -r requirements.txt
-exec $PELICAN $INPUTDIR -o $OUTPUTDIR -s $CONFFILE
+$PIP install -r requirements.txt
+$PELICAN $INPUTDIR -o $OUTPUTDIR -s $CONFFILE
