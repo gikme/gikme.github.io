c845bc4f628a8ed49cf5b9d4b0bb08cb5246d5b0
Vadim Lopatyuk <qnub.ru@gmail.com>
2015-02-01 00:29:17 +0600

    Another publish fox

--- publish.sh
+++ publish.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-GIT_ROOT=/var/www/gik.me/pelican_site
-VIRTUALENV=/var/www/gik.me/.env/bin
-PELICAN=${VIRTUAENV}/pelican
-PY=${VIRTUAENV}/python
-PIP=${VIRTUAENV}/pip
-INPUTDIR=${GIT_ROOT}/site/content
-OUTPUTDIR=${GIT_ROOT}/site/output
-CONFFILE=${GIT_ROOT}/site/publishconf.py
+GIT_ROOT="/var/www/gik.me/pelican_site"
+VIRTUALENV="/var/www/gik.me/.env/bin"
+PELICAN="${VIRTUAENV}/pelican"
+PY="${VIRTUAENV}/python"
+PIP="${VIRTUAENV}/pip"
+INPUTDIR="${GIT_ROOT}/site/content"
+OUTPUTDIR="${GIT_ROOT}/site/output"
+CONFFILE="${GIT_ROOT}/site/publishconf.py"
 
 
 cd $GIT_ROOT
