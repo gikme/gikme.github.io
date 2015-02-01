e20f5ccfd9fc07baa7461b70d50fc96cea856d4e
Vadim Lopatyuk <qnub.ru@gmail.com>
2015-02-01 11:31:22 +0600

    fixed instagram button style

--- site/sidebar_items.py
+++ site/sidebar_items.py
@@ -7,8 +7,9 @@ SIDEBAR_ITEMS = (
     <a href="http://feeds.feedburner.com/gikme"> <img src="http://img252.imageshack.us/img252/9795/podcastfeed.png" /> </a>
     <a href="https://itunes.apple.com/ru/podcast?id=597105136"> <img src="http://img822.imageshack.us/img822/7156/itunesfeed.png" /></a>
     <style>.ig-b- { display: inline-block; }
-    .ig-b- img { visibility: hidden; }
-    .ig-b-:hover { background-position: 0 -60px; } .ig-b-:active { background-position: 0 -120px; }
+    .ig-b- img { visibility: hidden; margin-top: 0;}
+    .ig-b-:hover { background-position: 0 -60px; }
+    .ig-b-:active { background-position: 0 -120px; }
     .ig-b-v-24 { width: 137px; height: 24px; background: url(//badges.instagram.com/static/images/ig-badge-view-sprite-24.png) no-repeat 0 0; }
     @media only screen and (-webkit-min-device-pixel-ratio: 2), only screen and (min--moz-device-pixel-ratio: 2), only screen and (-o-min-device-pixel-ratio: 2 / 1), only screen and (min-device-pixel-ratio: 2), only screen and (min-resolution: 192dpi), only screen and (min-resolution: 2dppx) {
     .ig-b-v-24 { background-image: url(//badges.instagram.com/static/images/ig-badge-view-sprite-24@2x.png); background-size: 160px 178px; } }</style>
