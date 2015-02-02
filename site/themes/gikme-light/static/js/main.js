1dcfc8be480a91f43dcc2e34d79ab1dc0caa4e5d
Vadim Lopatyuk <qnub.ru@gmail.com>
2015-02-02 14:44:12 +0600

    Added fluidbox

--- site/themes/gikme-light/static/js/main.js
+++ site/themes/gikme-light/static/js/main.js
@@ -1,19 +1,28 @@
 'use strict';
 
-var showVK = function(){
+$( function(){
+  $( '.article__content img' ).each(function(){
+    var $this = $(this);
+    $this.wrap('<a href="' + $this.attr('src') + '" rel="lightbox"></a>');
+  });
 
-  VK.Widgets.Group(
-    "vk_groups",
-    {
-      mode: 2,
-      width: 220,
-      height: 400,
-      color1: 'FFFFFF',
-      color2: '2B587A',
-      color3: '5B7FA6'
-    },
-    48255349
-  );
-}
+  $('[rel="lightbox"]').fluidbox();
 
-showVK();
+  var showVK = function(){
+
+    VK.Widgets.Group(
+      "vk_groups",
+      {
+        mode: 2,
+        width: 220,
+        height: 400,
+        color1: 'FFFFFF',
+        color2: '2B587A',
+        color3: '5B7FA6'
+      },
+      48255349
+    );
+  }
+
+  showVK();
+});
