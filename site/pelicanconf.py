c9e122bf9016b5c1622632ee5404f148a4e8190c
Vadim Lopatyuk <qnub.ru@gmail.com>
2015-02-22 01:35:44 +0600

    enabled typogrify

--- site/pelicanconf.py
+++ site/pelicanconf.py
@@ -46,6 +46,7 @@ LOAD_CONTENT_CACHE = False
 
 # Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = True
+TYPOGRIFY = True
 
 THEME = './themes/gikme-light'
 # THEME = './themes/blue-penguin'
